140e22972a81a4c68d4076c9c5406b6b1e80db53
YugendarM <yugendar2411@gmail.com>
2024-06-20 11:33:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-2xl bg-blue-200'>
+      <p className='bg-blue-400 text-2xl'> app</p>
+    </div>
+  )
+}
+
+export default App
